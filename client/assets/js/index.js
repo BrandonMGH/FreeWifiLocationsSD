bafa8e6e699bae44133887043dea03d9bf6e6216
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-13 11:18:20 -0800

    was able to change the map display by grabbing the dom element attribute and changing its value in the index.js

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,9 +1,11 @@
 const testButton = document.getElementById("testButton")
 
+
 let fetchGet = function (locationInfo) {
   fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&location=${locationInfo}`)
 .then( res => res.json())
 .then(location => console.log(location))
+document.getElementById("imgTest").src = "https://www.mapquestapi.com/staticmap/v4/getmap?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&size=600,400&zoom=11&center=32.81576,-117.163817"
 }
 
 
