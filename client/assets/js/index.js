e4347a333a20bbfb7d834b2ac8886f861f27cf86
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-14 13:38:38 -0800

    added geolocation function to index.js for use in placement in map url

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,11 +1,29 @@
-const testButton = document.getElementById("testButton")
 
+
+const testButton = document.getElementById("testButton")
+const mapQuestURL = "https://www.mapquestapi.com/staticmap/v4/getmap?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&size=600,400&zoom=11&center=42.81576,-117.163817"
+
+let getLocation = function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(printLocation);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+let printLocation = function (position) {
+  let latitude = position.coords.latitude
+  let longitude = position.coords.longitude
+  console.log(`Latitude: ${latitude}, Longitutde: ${longitude}`)
+  
+} 
 
 let fetchGet = function (locationInfo) {
   fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&location=${locationInfo}`)
 .then( res => res.json())
 .then(location => console.log(location))
-document.getElementById("imgTest").src = "https://www.mapquestapi.com/staticmap/v4/getmap?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&size=600,400&zoom=11&center=32.81576,-117.163817"
+document.getElementById("imgTest").src = mapQuestURL
+getLocation();
 }
 
 
