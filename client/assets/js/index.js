8e50f1ff5729acec553c25c20eb73c98b1088f01
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-14 16:06:16 -0800

    added a current location and custom location button and changed the logic inside of the index.js file correspondingly

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,7 +1,8 @@
 
 
-const testButton = document.getElementById("testButton")
-const mapQuestURL = "https://www.mapquestapi.com/staticmap/v4/getmap?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&size=600,400&zoom=11&center=42.81576,-117.163817"
+const currentLocButton = document.getElementById("")
+const customLocButton = document.getElementById("customLocButton")
+const mapQuestURL = "https://www.mapquestapi.com/staticmap/v4/getmap?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&size=600,400&zoom=11&center=4=32.81576,-117.163817"
 
 let getLocation = function() {
   if (navigator.geolocation) {
@@ -15,20 +16,19 @@ let printLocation = function (position) {
   let latitude = position.coords.latitude
   let longitude = position.coords.longitude
   console.log(`Latitude: ${latitude}, Longitutde: ${longitude}`)
-  
+  mapQuestURL = `https://www.mapquestapi.com/staticmap/v4/getmap?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&size=600,400&zoom=11&center=4=${latitude},${longitude}`
+  document.getElementById("imgTest").src = mapQuestURL
 } 
 
 let fetchGet = function (locationInfo) {
   fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&location=${locationInfo}`)
 .then( res => res.json())
 .then(location => console.log(location))
-document.getElementById("imgTest").src = mapQuestURL
-getLocation();
 }
 
 
-testButton.onclick = function () {
- let newVar = document.getElementById("testInput").value 
+customLocButton.onclick = function () {
+ let newVar = document.getElementById("customLocInput").value 
  console.log(newVar)
  fetchGet(newVar)
 }
