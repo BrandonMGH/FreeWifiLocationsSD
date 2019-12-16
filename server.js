43047ea405e699dcdb78a556bb849be7d731185c
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-15 17:07:38 -0800

    assigned Map Quest API key to a .env variable, installed dotenv package and set key to mapQuestAPIKey variable

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require('dotenv').config()
 const express = require("express")
 const fetch = require("node-fetch")
 const app = express();
 const PORT = 3000
+const mapQuestAPIKey = process.env.MAPQUEST_API_KEY
+const mapQuestAPIURL = `http://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestAPIKey}&location=San+Diego,CA`
+
 
 app.use(express.static("client/assets"));
 
 app.get("/MapQuestInfo", async (req, res) => {
-   const fetch_response = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&location=San+Diego,CA`)
+   const fetch_response = await fetch(mapQuestAPIURL)
    const json = await fetch_response.json(); 
    res.json(json)
 })
