74352f3a10869caaf0b19a0c7436087e5b766f2d
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-16 12:32:49 -0800

    got the leaflet template to load

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -2,13 +2,15 @@
 
 const currentLocButton = document.getElementById("currentLocButton")
 const customLocButton = document.getElementById("customLocButton")
+// const mymap = L.map('mapContainer').setView([0, 0], 0);
+// const mymarker = L.marker([0,0]).addTo(mymap)
 
+let mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 let latitude = "32.81576"
 let longitude = "-117.163817"
-let mapQuestStatic = `http://www.mapquestapi.com/staticmap/v5/map?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&type=map&size=600,400&locations=${latitude},${longitude}|marker-sm-50318A-1&scalebar=true&zoom=12&rand=1848547955`
 
-let getLocation = function() {
+let getLocation = function () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getCords);
   } else {
@@ -16,41 +18,37 @@ let getLocation = function() {
   }
 }
 
-let printMap = function () {
-  console.log(latitude, longitude)
-  console.log(mapQuestStatic)
-  document.getElementById("imgTest").src = mapQuestStatic
+let printMap = function (Lat, Long) {
+  mymap.setView([Lat, Long], 12)
+  mymarker.setLatLng([Lat, Long], 12)
 }
 
 let getCords = function (position) {
-   latitude = position.coords.latitude
-   longitude = position.coords.longitude
-  //  mapQuestStatic = `http://www.mapquestapi.com/staticmap/v5/map?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&type=map&size=600,400&locations=${latitude},${longitude}|marker-sm-50318A-1&scalebar=true&zoom=12&rand=1848547955`
+  latitude = position.coords.latitude
+  longitude = position.coords.longitude
   console.log(`Latitude: ${latitude}, Longitutde: ${longitude}`)
-  // printMap(); 
   fetchGet(latitude, longitude)
-} 
+  printMap(latitude, longitude)
+}
 
 let fetchGet = function (paramOne, paramTwo) {
   fetch(`http://localhost:3000/MapQuestInfo/${paramOne}/${paramTwo}`)
-.then( res => res.json())
-.then(data => {
-  let addressInfo = data.searchResults[0].fields
-  
-   
-     let address = addressInfo.address
-     let city = addressInfo.city
-     let state = addressInfo.state
-     let zipCode = addressInfo.postal_code
-    
-    console.log(addressInfo)
-    document.getElementById("addressLineOne").innerHTML = address
-    document.getElementById("addressLineTwo").innerHTML = `${city}, ${state}, ${zipCode}`
-  
-})
+    .then(res => res.json())
+    .then(data => {
+      let addressInfo = data.searchResults[0].fields
+      let address = addressInfo.address
+      let city = addressInfo.city
+      let state = addressInfo.state
+      let zipCode = addressInfo.postal_code
+      console.log(addressInfo)
+      document.getElementById("addressLineOne").innerHTML = address
+      document.getElementById("addressLineTwo").innerHTML = `${city}, ${state}, ${zipCode}`
+
+    })
 }
 
 
 currentLocButton.onclick = function () {
   getLocation()
 }
+
