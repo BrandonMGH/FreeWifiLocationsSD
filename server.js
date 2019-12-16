6f9fa7b758eafd680dfad3301bb9971fb06e09f0
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-15 16:52:11 -0800

    able to get a response from MapQuestApi using the node express server by initiating a request from the front end

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express")
-const axios = require("axios")
-const path = require("path")
+const fetch = require("node-fetch")
 const app = express();
 const PORT = 3000
 
 app.use(express.static("client/assets"));
 
-app.get("/MapQuestInfo", (req, res) => {
-    axios(`http://www.mapquestapi.com/geocoding/v1/address?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&location=San+Diego,CA`)
-        .then(data => console.log(data))
+app.get("/MapQuestInfo", async (req, res) => {
+   const fetch_response = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=Caw3YlgZ6hKw9GVS49dNHOVfR1mqbGCy&location=San+Diego,CA`)
+   const json = await fetch_response.json(); 
+   res.json(json)
 })
 
 app.listen(PORT, () => {
