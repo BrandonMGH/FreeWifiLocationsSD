f05217f66f6018418a5c30ab522930c1079e4a10
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-15 21:50:04 -0800

    was able to pass coordinates from the front end to a back end route and pass those coordinates as a request to a mapQuestAPI route that returned a jason response containing location data

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const app = express();
 const PORT = 3000
 
 let mapQuestAPIKey = process.env.MAPQUEST_API_KEY
-let mapQuestAPIURL = `http://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestAPIKey}&location=San+Diego,CA`
+let mapQuestAPIURL = `http://www.mapquestapi.com/search/v2/radius?key=${mapQuestAPIKey}&maxMatches=4&origin=33.2398592,-117.2692321`
 
 
 app.use(express.static("client/assets"));
 
 app.get("/MapQuestInfo/:lat/:long", async (req, res) => {
     console.log(req.params)
+    let latitude = req.params.lat
+    let longitude = req.params.long
+    let mapQuestAPIURL = `http://www.mapquestapi.com/search/v2/radius?key=${mapQuestAPIKey}&maxMatches=4&origin=${latitude},${longitude}`
     const fetch_response = await fetch(mapQuestAPIURL)
     const json = await fetch_response.json();
     res.json(json)
