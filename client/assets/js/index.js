77e64c8a2f4017a95cae00ebfd71a2cf781b315a
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-17 15:41:26 -0800

    imported old leaflet map code from old project to get the leaflet map up and running in this app

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -6,6 +6,10 @@ const customLocButton = document.getElementById("customLocButton")
 // const mymarker = L.marker([0,0]).addTo(mymap)
 
 let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);;
 
 let latitude = "32.81576"
 let longitude = "-117.163817"
