579466d8f34ff39c3a87fd759ce57270efafb598
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-29 14:04:32 -0800

    threw all the leaftlet code inside of the fetch response and used the response from the fetch function to change the coordinates using a combination of a get/put and page refresh.  Need to set up the leaflet routing now inside of the mapRoute.js

--- client/assets/js/mapRoute.js
+++ client/assets/js/mapRoute.js
@@ -1,20 +1,37 @@
+// let currentLat = 32.81576
+// let currentLong = -117.163817
 
-const mymap = L.map('mapRouteId', {closePopupOnClick: false}).setView([32.81576, -117.163817], 12);
-const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(mymap);;
+// const mymap = L.map('mapRouteId', {closePopupOnClick: false}).setView([32.81576, -117.163817], 12);
+// const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+// const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+// const tiles = L.tileLayer(tileUrl, { attribution });
+// tiles.addTo(mymap);;
 // const myMarker= L.marker([currentLat, currentLong]).addTo(mymap);
 
-let printMap = function (Lat, Long) {
-    mymap.setView([Lat, Long], 12)
-    // myMarker.setLatLng([Lat, Long], 12).bindPopup("<b>You are here!</b>.").openPopup();
-  }
-printMap(33.2417075, -117.2692269)
-
-const urlParams = new URLSearchParams(window.location.search);
-const myParam = urlParams.get('myParam');
+// let printMap = function (Lat, Long) {
+//     mymap.setView([Lat, Long], 12)
+//     myMarker.setLatLng([Lat, Long], 12).bindPopup("<b>You are here!</b>.").openPopup();
+//   }
+// // printMap(currentLat, currentLong)
 
 fetch(`http://localhost:3000/api/routeInfo`)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+  console.log(data)
+  let currentLat = data.latitude
+  let currentLong = data.longitude
+  const mymap = L.map('mapRouteId', {closePopupOnClick: false}).setView([32.81576, -117.163817], 12);
+  const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+  const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  const tiles = L.tileLayer(tileUrl, { attribution });
+  tiles.addTo(mymap);;
+  const myMarker= L.marker([currentLat, currentLong]).addTo(mymap);
+
+  let printMap = function (Lat, Long) {
+    mymap.setView([Lat, Long], 12)
+    myMarker.setLatLng([Lat, Long], 12).bindPopup("<b>You are here!</b>.").openPopup();
+  }
+
+  printMap(currentLat, currentLong)
+
+})
